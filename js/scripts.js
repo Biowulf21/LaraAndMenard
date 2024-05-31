ce8249592d6c8f6e6fb305857e5493a0b6cd7e30
James Jilhaney <jamesjilhaney21@gmail.com>
2024-05-31 21:20:06 +0800

    feat: remove unecessary invite code

--- js/scripts.js
+++ js/scripts.js
@@ -225,32 +225,26 @@ $(document).ready(function() {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        }
-        else {
-            $.ajax({
-                type: 'POST',
-                url: 'https://script.google.com/macros/s/AKfycbycp6JshH_SCPKOpVd_KpCJe16Y1dNFAOjLx0IkTcTFZBA4DP5qirxSuFjSGbKmgx9z/exec',
-                data: data,
-                dataType: 'json',
-                success: function(data) {
-                    console.log(data);
-                    if (data.result === "error") {
-                        $('#alert-wrapper').html(alert_markup('danger', data.message));
-                    } else {
-                        $('#alert-wrapper').html('');
-                        $('#rsvp-modal').modal('show');
-                    }
-                },
-                error: function(data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                },
-                redirect: 'follow'
-            });
-        }
+        $.ajax({
+            type: 'POST',
+            url: 'https://script.google.com/macros/s/AKfycbycp6JshH_SCPKOpVd_KpCJe16Y1dNFAOjLx0IkTcTFZBA4DP5qirxSuFjSGbKmgx9z/exec',
+            data: data,
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                if (data.result === "error") {
+                    $('#alert-wrapper').html(alert_markup('danger', data.message));
+                } else {
+                    $('#alert-wrapper').html('');
+                    $('#rsvp-modal').modal('show');
+                }
+            },
+            error: function(data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            },
+            redirect: 'follow'
+        });
 
     });
 
